a0ea289abfdb9c19bb80539dd0059700edd581a7
lovelife <lovelife@kaist.ac.kr>
2019-07-21 21:45:22 +0900

    Add naive test code for type.js. Need revisement

---
+++ tests/test_type.js
@@ -0,0 +1,27 @@
+const check = require("../src/type.js")
+
+function Func1(f) {
+}
+function Func2(f) {
+}
+class Person {
+  constructor(height, weight) {
+    this.height = height;
+    this.weight = weight;
+  }
+}
+const str1 = "str1";
+const str2 = "str2";
+const str3 = "str3";
+const num1 = 1;
+const num2 = 2;
+
+const obj = [1,2,3,"3jkjlkfda"]
+const jjj = {1: 2, 4:5}
+var p = new Person();
+var p1 = new Person();
+check.type(p, p1)
+check.type(str1, str2)
+check.type(str1, str2, str3)
+check.type(num1, num2)
+console.log("dhidhidh")
