0986be5b41e338cc8ef850428cdbcb4b2bfd554e
Junseop <gaon0403@gmail.com>
2019-07-22 20:53:44 +0900

    Add debugPrint function

--- src/print.js
+++ src/print.js
@@ -1,11 +1,65 @@
-import {} from '../node.js';
+var fs = require('fs');
 
-const print = (...args) => {
-  args.forEach((arg) => {
-    console.log(args)
-    return;
-  })
+function get_line(latestStack, callback) {
+  var data = fs.readFileSync(latestStack.getFileName(), 'utf8');
+  var lines = data.split('\n');
+
+  if (+latestStack.getLineNumber() - 1 > lines.length) {
+    throw new Error('File end reached without finding line');
+  }
+
+  var line = lines.slice(+latestStack.getLineNumber() - 1).join('\n');
+  var argPart = line.slice(latestStack.getColumnNumber() - 1);
+
+  var startIdx = argPart.indexOf('(');
+  var leftParen = 1;
+  var endIdx = startIdx;
+
+  while (leftParen > 0) {
+    endIdx++;
+    if (argPart[endIdx] === '(') {
+      leftParen++;
+    } else if (argPart[endIdx] === ')') {
+      leftParen--;
+    }
+  }
+  var argNames = argPart.slice(startIdx + 1, endIdx).split(/\s*,\s*/);
+
+  callback(null, line.split('\n')[0], argNames);
 }
 
+function check(...args) {
+  var checkobj = new Error();
 
-export {print};
+  Error.prepareStackTrace = function(err, stack) {
+    return stack;
+  };
+  Error.captureStackTrace(checkobj, check);
+
+  latestStack = checkobj.stack[0];
+
+  get_line(latestStack, function(err, line, argNames) {
+    console.log('========= The Amazing Library: Check.js =========');
+    console.log(`Requested line: ${line.trim()}`);
+
+    argNames.forEach((e, i) => {
+      console.log(`${e}: ${args[i]}`);
+    });
+
+    console.log('=================================================');
+  });
+}
+
+function testfunc() {
+  var myVal1 = 1234;
+  var myVal2 = 4567;
+  var soyoung = 777;
+
+  console.log('I will test check function!');
+  check(myVal1, myVal2, Number(10), soyoung);
+  console.log('Done...?');
+}
+
+testfunc();
+
+module.exports.debugPrint = check;
