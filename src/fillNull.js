d3619c7f8ba4421fb9978c6b50d6111714e0ee25
lovelife <lovelife@kaist.ac.kr>
2019-07-28 23:11:06 +0900

    Implement fillNull.js and fillUndefined.js

---
+++ src/fillNull.js
@@ -0,0 +1,25 @@
+module.exports = {
+  fillNull: function(obj, toFill) {
+    // isDict(): check if object type is dictonary, without being null or undefined.
+    function isDict(o){
+      return (obj == 'null' || obj.constructor !== Object) ? false : true
+    }
+    function fillInside(o, toFill){
+      Object.keys(o).forEach(key => {
+        if (isDict(o[key])) {
+          o[key] = fillInside(o)
+        }
+        else if (o[key] === null) {
+          o[key] = toFill
+        }
+      })
+      return o
+    }
+    if (isDict(obj)){
+      return fillInside(obj, toFill)
+    }
+    else {
+      console.log(`object is ${obj}. The object has to be instanceOf object to execute fillNull`)
+    }
+  }
+}
